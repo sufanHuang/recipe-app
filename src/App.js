9617d086bf850061a84939be251062457cdce427
sufanHuang <sufanhk@gmail.com>
2019-05-09 12:18:07 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import config from './settings/secrets'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const { API_KEY } = config;
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (event) => {
+    const recipeName = event.target.elements.recipeName.value;
+    event.preventDefault();
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const results = await fetch(`${proxy}http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=9`);
+
+    const data = await results.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  }
+
+ getPopularRecipe = async ()=>{
+     const proxy = 'https://cors-anywhere.herokuapp.com/'
+     const results = await fetch(`${proxy}http://food2fork.com/api/search?key=${API_KEY}&count=9`);
+     const data = await results.json();
+     this.setState({ recipes: data.recipes });
+
+ }
+ componentDidMount=()=>{
+   this.getPopularRecipe()
+ }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+            <h3 className="App-title text-info">Search with Your Favorite Ingredient</h3>
+
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
 }
 
 export default App;
