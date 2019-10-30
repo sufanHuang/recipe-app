58bfc95f585ded5d767162fce09888e288f8afd4
sufanHuang <sufanhk@gmail.com>
2019-10-30 17:28:42 -0500

    modifying css

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -8,7 +8,7 @@ const Recipes = (props)=> {
             <div className="row">
                 {props.recipes.map((recipe) => {
                     return (
-                        <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }}>
+                        <div key={recipe.title}   style={{ marginBottom:"2rem" }}>
                             <div className="recipes__box">
                                 <img
                                     className="recipe__box-img"
